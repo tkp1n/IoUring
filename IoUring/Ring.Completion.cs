549612ffa64ebbb4312f58b5994f2bc803b2162a
Nicolas Portmann <ndportmann@gmail.com>
2019-11-06 17:22:24 +0100

    Add single blocking read

--- IoUring/Ring.Completion.cs
+++ IoUring/Ring.Completion.cs
@@ -17,6 +17,17 @@ namespace IoUring
         /// <returns>Whether a Completion Queue Event was observed</returns>
         public bool TryRead(ref Completion result) => _cq.TryRead(ref result);
 
+        /// <summary>
+        /// Reads, blocking if required, for a Completion Queue Event.
+        /// </summary>
+        /// <returns>The read Completion Queue Event</returns>
+        public Completion Read()
+            => _cq.Read(_ringFd.DangerousGetHandle().ToInt32());
+
+        /// <summary>
+        /// Reads, blocking if required, for as many Completion Queue Events as fit the provided span.
+        /// </summary>
+        /// <param name="results">Buffer for the Completion Queue Events</param>
         public void Read(Span<Completion> results)
             => _cq.Read(_ringFd.DangerousGetHandle().ToInt32(), results);
     }
