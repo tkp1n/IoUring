99754386a798387bd37cbcef1d8ce98703cd2ba5
Nicolas Portmann <ndportmann@gmail.com>
2019-12-19 11:13:09 +0100

    Add documentation

--- IoUring/SubmissionOption.cs
+++ IoUring/SubmissionOption.cs
@@ -5,8 +5,19 @@ namespace IoUring
     [Flags]
     public enum SubmissionOption : byte
     {
+        /// <summary>
+        /// Marks this submission as independent of other submissions.
+        /// </summary>
         None = 0x00,
+        
+        /// <summary>
+        /// Wait for previously submitted items before the current one is issued.
+        /// </summary>
         Drain = 0x01, // IOSQE_IO_DRAIN
+        
+        /// <summary>
+        /// Marks items of a chain that must be executed sequentially.
+        /// </summary>
         Link = 0x02 // IOSQE_IO_LINK
     }
 }
