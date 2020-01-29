62f62da4a5346e887bf4e28513de7ad066d299b2
Nicolas Portmann <ndportmann@gmail.com>
2020-01-29 21:16:39 +0100

    Add outbound connection to test app

---
+++ tests/IoUring.TestApp/Controllers/RandomController.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IoUring.TestApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RandomController : ControllerBase
+    {
+        private readonly IHttpClientFactory _clientFactory;
+
+        public RandomController(IHttpClientFactory clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            using var client = _clientFactory.CreateClient();
+            var result = await client.GetAsync("https://api.chucknorris.io/jokes/random");
+            return await result.Content.ReadAsStringAsync();
+        }
+    }
+}
--- tests/IoUring.TestApp/Startup.cs
+++ tests/IoUring.TestApp/Startup.cs
@@ -19,6 +19,7 @@ namespace IoUring.TestApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHttpClient();
             services.AddIoUringTransport();
             services.AddControllers();
         }
