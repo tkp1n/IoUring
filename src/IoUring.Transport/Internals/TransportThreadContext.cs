468fe0f10271abd711321e698144bc3c102ce038
Nicolas Portmann <ndportmann@gmail.com>
2020-02-03 17:40:38 +0100

    Notify transport thread correctly

--- src/IoUring.Transport/Internals/TransportThreadContext.cs
+++ src/IoUring.Transport/Internals/TransportThreadContext.cs
@@ -1,5 +1,6 @@
 using System.Buffers;
 using System.Collections.Concurrent;
+using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using System.Threading;
 using IoUring.Transport.Internals.Metrics;
@@ -38,9 +39,20 @@ namespace IoUring.Transport.Internals
 
         public MemoryPool<byte> MemoryPool { get; }
 
-        public unsafe void Unblock()
+        public unsafe void Notify()
         {
+            if (!BlockingMode)
+            {
+                // If the transport thread is not (yet) in blocking mode, we have the guarantee, that it will read 
+                // from the queues one more time before actually blocking. Therefore, it is safe not to notify now.
+                return;
+            }
+
+            // The transport thread reported he is (probably still) blocking. We therefore must notify it via writing
+            // to the eventfd established for that purpose.
+
             IoUringTransportEventSource.Log.ReportEventFdWrite();
+            Debug.WriteLine("Attempting to unblock thread");
 
             byte* val = stackalloc byte[sizeof(ulong)];
             Unsafe.WriteUnaligned(val, 1UL);
