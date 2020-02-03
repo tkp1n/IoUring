164e2ee3685e7c1156da99dd1a65301aa30a438a
Ilja Nosik <ilja.nosik@outlook.com>
2020-02-03 11:41:13 +0100

    Fix OS compatibility detection
    This was an oversight I guess.

--- src/IoUring.Transport/Internals/OsCompatibility.cs
+++ src/IoUring.Transport/Internals/OsCompatibility.cs
@@ -14,7 +14,7 @@ namespace IoUring.Transport.Internals
                 
                 var osVersion = Environment.OSVersion.Version;
                 if (osVersion.Major > 5) return true;
-                return osVersion.Major == 5 && osVersion.Major >= 4;
+                return osVersion.Major == 5 && osVersion.Minor >= 4;
             }
         }
     }
