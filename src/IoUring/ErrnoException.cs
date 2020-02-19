bddeb225020eb3f3e2fe4638424f29004c43445a
Tom Deseyn <tom.deseyn@gmail.com>
2020-02-19 11:19:11 +0100

    ErrnoException: make message human friendly

--- src/IoUring/ErrnoException.cs
+++ src/IoUring/ErrnoException.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Runtime.InteropServices;
+using static Tmds.Linux.LibC;
 
 namespace IoUring
 {
@@ -9,9 +11,19 @@ namespace IoUring
     {
         public int Errno { get; }
 
-        public ErrnoException(int errno) : base($"ERRNO: {errno}")
+        public ErrnoException(int errno) : base(GetErrorMessage(errno))
         {
             Errno = errno;
+        }
+
+        private unsafe static string GetErrorMessage(int errno)
+        {
+            const int bufferLength = 1024;
+            byte* buffer = stackalloc byte[bufferLength];
+
+            int rv = strerror_r(errno, buffer, bufferLength);
+
+            return rv == 0 ? Marshal.PtrToStringAnsi((IntPtr)buffer)! : $"ERRNO: {errno}";
         }
     }
 }
